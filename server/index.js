58f41c1c0ef50f0cc5a0d2b54078b5ace786e53e
VitakSharper <v.banard@gmail.com>
2019-06-27 20:23:44 +0200

    Create node express server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
-const express = require('express')
 const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
-
+const {Nuxt, Builder} = require('nuxt')
+const {app, server} = require('./app.js')
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
@@ -11,7 +9,7 @@ async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const {host, port} = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
@@ -25,10 +23,12 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+  server.listen(port, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
   })
 }
+
 start()
