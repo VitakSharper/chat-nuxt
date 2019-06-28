b06c721749c67b1d4fef25ee90f856b7c63b1f34
VitakSharper <v.banard@gmail.com>
2019-06-28 22:53:59 +0200

    End realisation of the chat

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ io.on('connection', socket => {
     });
 
     cb({userId: socket.id});
+    io.to(data.room).emit('updateUsers', users.getByRoom(data.room));
     socket.emit('newMessage', m('admin', `Bienvenue ${data.name}`));
     socket.broadcast
       .to(data.room)
@@ -36,8 +37,24 @@ io.on('connection', socket => {
       io.to(user.room).emit('newMessage', m(user.name, data.text, data.id))
     }
     cb()
-  })
+  });
 
+  socket.on('userLeft', (id, cb) => {
+    const user = users.remove(id);
+    if (user) {
+      io.to(user.room).emit('updateUsers', users.getByRoom(user.room));
+      io.to(user.room).emit('newMessage', m('admin', `L'utilisateur ${user.name} a quitté la conversation.`))
+    }
+    cb()
+  });
+
+  socket.on('disconnect', () => {
+    const user = users.remove(socket.id);
+    if (user) {
+      io.to(user.room).emit('updateUsers', users.getByRoom(user.room));
+      io.to(user.room).emit('newMessage', m('admin', `L'utilisateur ${user.name} a quitté la conversation.`))
+    }
+  })
 });
 
 module.exports = {
