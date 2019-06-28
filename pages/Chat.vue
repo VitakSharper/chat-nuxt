7feeb546b7c4d328481f1215f8dbc6d303cef78f
BANARD Victor <VBANARD@fr.scc.com>
2019-06-28 17:00:10 +0200

    Create message.vue component
    and a test message not form a admin just for test;

--- pages/Chat.vue
+++ pages/Chat.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="c-wrap">
         <div class="c-chat">
-            <ul>
-                <li v-for="mes in messages" :key="mes.text">{{mes.text}}</li>
-            </ul>
+            <app-message
+                    v-for="m in messages" :key="m.text"
+                    :name="m.name"
+                    :text="m.text"
+                    owner
+            />
         </div>
         <div class="c-form"></div>
     </div>
@@ -11,8 +14,12 @@
 
 <script>
   import { mapState } from 'vuex'
+  import Message from '../components/Message.vue'
 
   export default {
+    components: {
+      appMessage: Message
+    },
     middleware: ['chat'],
     head() {
       return {
@@ -42,6 +49,12 @@
     }
 
     .c-chat {
-
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 80px;
+        padding: 1rem;
+        overflow-y: auto;
     }
 </style>
